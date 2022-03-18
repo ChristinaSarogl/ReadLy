be3a9c547aa6d0cc97fe66cff191c2a1fb7e9391
Christina Saroglaki <1801448@csl-student.wlv.ac.uk>
2022-03-18 10:33:27 +0000

    Removed decoration from links

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -14,6 +14,8 @@
 	<!-- Font Awesome Icon Library -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	
+	<link rel="stylesheet" href="<?php echo base_url('css/main.css'); ?>">
+	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <title>BookHood</title>
