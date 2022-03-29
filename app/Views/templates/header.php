a0529871815772c72df3ae06d35a44e6fc1413fc
Christina Saroglaki <1801448@csl-student.wlv.ac.uk>
2022-03-29 13:53:20 +0100

    This fixes #15

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -221,7 +221,7 @@
         </button>
 		
         <div class="collapse navbar-collapse" id="navbarContent">
-			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0 align-items-center">
 				<li class="nav-item">
 					<a class="nav-link" aria-current="page" href="<?php echo base_url() ?>/home">Home</a>
 				</li>
@@ -245,11 +245,11 @@
 					<a class="nav-link" aria-current="page" href="<?php echo base_url() ?>/find-bookstore">Find Bookstore</a>
 				</li>
 				<?php if(!session()->get('isLoggedIn')): ?>
-					<li class="nav-item d-none d-lg-flex">
+					<li class="nav-item d-none d-md-flex">
 						<a class="nav-link" aria-current="page" href="<?php echo base_url() ?>/login">Login</a>
 					</li>
 				<?php else: ?>
-					<li class="nav-item d-none d-lg-flex">
+					<li class="nav-item d-none d-md-flex">
 						<a class="nav-link" aria-current="page" href="<?php echo base_url() ?>/profile/<?php echo session()->get('id') ?>">Profile</a>
 					</li>
 				<?php endif ?>
